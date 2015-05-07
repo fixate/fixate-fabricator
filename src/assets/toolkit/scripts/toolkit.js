4133458180489a2a2e839446193f3ef9bb4d4536
Larry Botha <larry@fixate.it>
2015-05-08 00:34:51 +0200

    add angular app and dependencies

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -4,4 +4,18 @@
 
 'use strict';
 
+angular.module('f', [
+  'ngAnimate'
+])
+  // we need to differentiate angular's properties from handlebars
+  .config(
+    ['$interpolateProvider',
+    function($interpolateProvider) {
+      $interpolateProvider.startSymbol('{[{');
+      $interpolateProvider.endSymbol('}]}');
+    }
+  ]);
+
+require('./animations');
 require('./layout');
+require('./progress');
