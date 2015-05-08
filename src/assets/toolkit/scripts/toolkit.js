8037527a04eb6f0d1729b312c5b3e19c572b9c80
Larry Botha <larry@fixate.it>
2015-05-08 14:55:45 +0200

    add some libs - need to be moved to fabricator

--- src/assets/toolkit/scripts/toolkit.js
+++ src/assets/toolkit/scripts/toolkit.js
@@ -4,18 +4,9 @@
 
 'use strict';
 
-angular.module('f', [
-  'ngAnimate'
-])
-  // we need to differentiate angular's properties from handlebars
-  .config(
-    ['$interpolateProvider',
-    function($interpolateProvider) {
-      $interpolateProvider.startSymbol('{[{');
-      $interpolateProvider.endSymbol('}]}');
-    }
-  ]);
-
+require('./utils');
+require('./angular-module');
 require('./animations');
 require('./layout');
 require('./progress');
+
