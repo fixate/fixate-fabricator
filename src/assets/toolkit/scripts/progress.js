985e5fbb24480a0be39db0fb3e01c7bf6db195c5
Larry Botha <larry@fixate.it>
2015-05-08 08:58:31 +0200

    improve timer class name

--- src/assets/toolkit/scripts/progress.js
+++ src/assets/toolkit/scripts/progress.js
@@ -1,34 +1,34 @@
 ;(function() {
-  var Trigger = (function() {
-    var Trigger = function(triggerEl) {
+  var TimedClassName = (function() {
+    var TimedClassName = function(triggerEl) {
       this.trigger = triggerEl;
       this.target = document.querySelector(this.trigger.getAttribute('data-target'));
-      this.targetClass = this.trigger.getAttribute('data-target-class');
-      this.timeout = this.trigger.getAttribute('data-trigger') || 2000;
+      this.targetClass = this.trigger.getAttribute('data-timed-class');
+      this.timeout = this.trigger.getAttribute('data-timeout') || 2000;
       this._init();
     }
 
-    Trigger.prototype._init = function() {
+    TimedClassName.prototype._init = function() {
       document.addEventListener('click', this._handleClick.bind(this));
     }
 
-    Trigger.prototype._handleClick = function(e) {
+    TimedClassName.prototype._handleClick = function(e) {
       if (e.target === this.trigger) {
         this._addClass();
         setTimeout(this._removeClass.bind(this), this.timeout);
       }
     }
 
-    Trigger.prototype._addClass = function() {
+    TimedClassName.prototype._addClass = function() {
       this.target.classList.add(this.targetClass);
     }
 
-    Trigger.prototype._removeClass = function() {
+    TimedClassName.prototype._removeClass = function() {
       this.target.classList.remove(this.targetClass);
     }
 
-    return Trigger;
+    return TimedClassName;
   })();
 
-  new Trigger(document.querySelector('[data-trigger]'));
+  new TimedClassName(document.querySelector('[data-timed-class]'));
 })();
