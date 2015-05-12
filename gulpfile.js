580d4aa84647369ccfa1b3edda2fdba0d4f26641
Larry Botha <larry@fixate.it>
2015-05-12 12:59:25 +0200

    update gulpfile to reflect project asset structure

--- gulpfile.js
+++ gulpfile.js
@@ -19,20 +19,21 @@ var webpack = require('webpack');
 
 // configuration
 var config = {
-	dev: gutil.env.dev,
-	src: {
-		scripts: {
-			fabricator: './src/assets/fabricator/scripts/fabricator.js',
-			toolkit: './src/assets/toolkit/scripts/toolkit.js'
-		},
-		styles: {
-			fabricator: 'src/assets/fabricator/styles/fabricator.scss',
-			toolkit: 'src/assets/toolkit/styles/*.scss'
-		},
-		images: 'src/assets/toolkit/images/**/*',
-		views: 'src/toolkit/views/*.html'
-	},
-	dest: 'dist'
+  dev: gutil.env.dev,
+  src: {
+    scripts: {
+      fabricator: './src/assets/fabricator/scripts/fabricator.js',
+      toolkit: './src/assets/toolkit/scripts/toolkit.js'
+    },
+    styles: {
+      fabricator: 'src/assets/fabricator/styles/fabricator.scss',
+      toolkit: 'src/assets/toolkit/assets/css/scss/style.scss'
+    },
+    images: 'src/assets/toolkit/assets/img/**/*',
+    fonts: 'src/assets/toolkit/assets/fnt/**/*',
+    views: 'src/toolkit/views/*.html',
+  },
+  dest: 'dist'
 };
 
 
@@ -43,32 +44,32 @@ var webpackCompiler = webpack(webpackConfig);
 
 // clean
 gulp.task('clean', function (cb) {
-	del([config.dest], cb);
+  del([config.dest], cb);
 });
 
 
 // styles
 gulp.task('styles:fabricator', function () {
-	return gulp.src(config.src.styles.fabricator)
-		.pipe(sass({
-			errLogToConsole: true
-		}))
-		.pipe(prefix('last 1 version'))
-		.pipe(gulpif(!config.dev, csso()))
-		.pipe(rename('f.css'))
-		.pipe(gulp.dest(config.dest + '/assets/fabricator/styles'))
-		.pipe(gulpif(config.dev, reload({stream:true})));
+  return gulp.src(config.src.styles.fabricator)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(prefix('last 1 version'))
+    .pipe(gulpif(!config.dev, csso()))
+    .pipe(rename('f.css'))
+    .pipe(gulp.dest(config.dest + '/assets/fabricator/styles'))
+    .pipe(gulpif(config.dev, reload({stream:true})));
 });
 
 gulp.task('styles:toolkit', function () {
-	return gulp.src(config.src.styles.toolkit)
-		.pipe(sass({
-			errLogToConsole: true
-		}))
-		.pipe(prefix('last 1 version'))
-		.pipe(gulpif(!config.dev, csso()))
-		.pipe(gulp.dest(config.dest + '/assets/toolkit/styles'))
-		.pipe(gulpif(config.dev, reload({stream:true})));
+  return gulp.src(config.src.styles.toolkit)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(prefix('last 1 version'))
+    .pipe(gulpif(!config.dev, csso()))
+    .pipe(gulp.dest(config.dest + '/assets/css'))
+    .pipe(gulpif(config.dev, reload({stream:true})));
 });
 
 gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
@@ -76,85 +77,94 @@ gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
 
 // scripts
 gulp.task('scripts', function (done) {
-	webpackCompiler.run(function (error, result) {
-		if (error) {
-			gutil.log(gutil.colors.red(error));
-		}
-		result = result.toJson();
-		if (result.errors.length) {
-			result.errors.forEach(function (error) {
-				gutil.log(gutil.colors.red(error));
-			});
-		}
-		done();
-	});
+  webpackCompiler.run(function (error, result) {
+    if (error) {
+      gutil.log(gutil.colors.red(error));
+    }
+    result = result.toJson();
+    if (result.errors.length) {
+      result.errors.forEach(function (error) {
+        gutil.log(gutil.colors.red(error));
+      });
+    }
+    done();
+  });
 });
 
 
 // images
 gulp.task('images', ['favicon'], function () {
-	return gulp.src(config.src.images)
-		.pipe(imagemin())
-		.pipe(gulp.dest(config.dest + '/assets/toolkit/images'));
+  return gulp.src(config.src.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.dest + '/assets/img'));
+});
+
+// fonts
+gulp.task('fonts', function () {
+  return gulp.src(config.src.fonts)
+    .pipe(gulp.dest(config.dest + '/assets/fnt'));
 });
 
 gulp.task('favicon', function () {
-	return gulp.src('./src/favicon.ico')
-		.pipe(gulp.dest(config.dest));
+  return gulp.src('./src/favicon.ico')
+    .pipe(gulp.dest(config.dest));
 });
 
 
 // assemble
 gulp.task('assemble', function (done) {
-	assemble();
-	done();
+  assemble();
+  done();
 });
 
 
 // server
 gulp.task('serve', function () {
 
-	browserSync({
-		server: {
-			baseDir: config.dest
-		},
-		notify: false,
-		logPrefix: 'FABRICATOR'
-	});
+  browserSync({
+    server: {
+      baseDir: config.dest
+    },
+    notify: false,
+    logPrefix: 'FABRICATOR'
+  });
 
-	/**
-	 * Because webpackCompiler.watch() isn't being used
-	 * manually remove the changed file path from the cache
-	 */
-	function webpackCache(e) {
-		var keys = Object.keys(webpackConfig.cache);
-		var key, matchedKey;
-		for (var keyIndex = 0; keyIndex < keys.length; keyIndex++) {
-			key = keys[keyIndex];
-			if (key.indexOf(e.path) !== -1) {
-				matchedKey = key;
-				break;
-			}
-		}
-		if (matchedKey) {
-			delete webpackConfig.cache[matchedKey];
-		}
-	}
+  /**
+   * Because webpackCompiler.watch() isn't being used
+   * manually remove the changed file path from the cache
+   */
+  function webpackCache(e) {
+    var keys = Object.keys(webpackConfig.cache);
+    var key, matchedKey;
+    for (var keyIndex = 0; keyIndex < keys.length; keyIndex++) {
+      key = keys[keyIndex];
+      if (key.indexOf(e.path) !== -1) {
+        matchedKey = key;
+        break;
+      }
+    }
+    if (matchedKey) {
+      delete webpackConfig.cache[matchedKey];
+    }
+  }
 
-	gulp.task('assemble:watch', ['assemble'], reload);
-	gulp.watch('src/**/*.{html,md,json,yml}', ['assemble:watch']);
+  gulp.task('assemble:watch', ['assemble'], reload);
+  gulp.watch('src/**/*.{html,md,json,yml}', ['assemble:watch']);
 
-	gulp.task('styles:fabricator:watch', ['styles:fabricator'], reload);
-	gulp.watch('src/assets/fabricator/styles/**/*.scss', ['styles:fabricator:watch']);
+  gulp.task('styles:fabricator:watch', ['styles:fabricator'], reload);
+  gulp.watch('src/assets/fabricator/styles/**/*.scss', ['styles:fabricator:watch']);
 
-	gulp.task('styles:toolkit:watch', ['styles:toolkit'], reload);
-	gulp.watch('src/assets/toolkit/styles/**/*.scss', ['styles:toolkit:watch']);
+  gulp.task('styles:toolkit:watch', ['styles:toolkit'], reload);
+  gulp.watch('src/assets/toolkit/assets/css/scss/**/*.scss', ['styles:toolkit:watch']);
 
-	gulp.task('scripts:watch', ['scripts'], reload);
-	gulp.watch('src/assets/{fabricator,toolkit}/scripts/**/*.js', ['scripts:watch']).on('change', webpackCache);
+  gulp.task('scripts:watch', ['scripts'], reload);
+  gulp.watch('src/assets/{fabricator,toolkit}/scripts/**/*.js', ['scripts:watch']).on('change', webpackCache);
 
-	gulp.task('images:watch', ['images'], reload);
-	gulp.watch(config.src.images, ['images:watch']);
+  gulp.task('images:watch', ['images'], reload);
+  gulp.watch(config.src.images, ['images:watch']);
+
+  gulp.task('fonts:watch', ['fonts'], reload);
+  gulp.watch(config.src.fonts, ['fonts:watch']);
 
 });
 
@@ -162,19 +172,21 @@ gulp.task('serve', function () {
 // default build task
 gulp.task('default', ['clean'], function () {
 
-	// define build tasks
-	var tasks = [
-		'styles',
-		'scripts',
-		'images',
-		'assemble'
-	];
+  // define build tasks
+  var tasks = [
+    'styles',
+    'scripts',
+    'images',
+    'fonts',
+    'assemble'
+  ];
 
-	// run build
-	runSequence(tasks, function () {
-		if (config.dev) {
-			gulp.start('serve');
-		}
-	});
+  // run build
+  runSequence(tasks, function () {
+    if (config.dev) {
+      gulp.start('serve');
+    }
+  });
 
 });
+
