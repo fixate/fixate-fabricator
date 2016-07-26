4cf747cb0c770c8c8eec5e381161dfee103c25de
Larry Botha <larry@fixate.it>
2016-07-26 09:51:50 +0200

    add setup script

---
+++ setup.js
@@ -0,0 +1,11 @@
+var shell = require("shelljs/global"),
+    exec = require("child_process").exec;
+
+if (!test('-L', 'src/assets/toolkit/assets')) {
+  echo('symlinking styleguide assets to src assets');
+  cd('src/assets/toolkit');
+  ln('-s', '../../../../src/assets', 'assets');
+  echo('assets symlinked');
+} else {
+  echo('styleguide symlinked to src assets');
+}
