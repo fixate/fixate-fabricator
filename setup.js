4585cf823f44e913edd051e34270238f7fdc4445
Larry Botha <larry@fixate.it>
2017-03-17 16:53:05 +0200

    create inline svg icons file if none present

--- setup.js
+++ setup.js
@@ -9,3 +9,11 @@ if (!test('-L', 'src/assets/toolkit/assets')) {
 } else {
   echo('styleguide symlinked to src assets. Check that src/assets/toolkit references the correct directory');
 }
+
+if (!test('-f', 'src/views/layouts/includes/inline-icons.svg.html')) {
+  echo('creating empty inline icons partial for styleguide');
+  touch('src/views/layouts/includes/inline-icons.svg.html');
+  echo('inline-icons.svg.html created');
+} else {
+  echo('inline-icons.svg.html ready');
+}
